1a165c1fc8ac670f4b169e6918f65efe62e3b96e
Ricky <henryats@gmail.com>
2023-04-12 19:58:26 +0100

    more comments added to the ms code

--- Exam_Rank_4/ms_with_comments.c
+++ Exam_Rank_4/ms_with_comments.c
@@ -2,57 +2,108 @@
 #include <sys/wait.h>
 #include <string.h>
 
-// A function to print a string to standard error (file descriptor 2)
+//simple putstr that write msg to stderr, and appends arg if theres one
 int	ft_putstr_2(char *msg, char *arg)
 {
-	while (*msg)
-		write(2, msg++, 1); // Write one character of the message at a time to stderr
-	if (arg)
-		while (*arg)
-			write(2, arg++, 1); // Write one character of the argument at a time to stderr
-	write(2, "\n", 1); // Write a newline character to stderr
-	return (1);
+    while (*msg)
+        write(2, msg++, 1);
+    if (arg)
+        while (*arg)
+            write(2, arg++, 1);
+    write(2, "\n", 1);
+    return (1);
 }
 
-// A function to execute a command with its arguments and environment variables
-// i is the index of the last argument before a semicolon or pipe character
-// tmp_fd is the file descriptor to use as stdin for the command
+// Function to execute a command
+// av is an array of command line arguments
+// i is the index of the last argument before a semicolon or pipe
+// tmp_fd is the file descriptor of the input stream
+// ev is an array of environment variables
 int	execute(char **av, int i, int tmp_fd, char **ev)
 {
-	av[i] = NULL; // Set the argument after the semicolon or pipe character to NULL to terminate the argument list
-	dup2(tmp_fd, STDIN_FILENO); // Replace stdin (file descriptor 0) with tmp_fd
-	close(tmp_fd); // Close tmp_fd
-	execve(av[0], av, ev); // Execute the command with its arguments and environment variables
-	return (ft_putstr_2("error:: cannot execute ", av[0])); // Print an error message if execve returns
+    // Set the i-th argument to NULL
+    av[i] = NULL;
+    // Replace the standard input with the given file descriptor
+    dup2(tmp_fd, STDIN_FILENO);
+    // Close the file descriptor
+    close(tmp_fd);
+    // Execute the command with the given arguments and environment variables
+    // If successful, this function does not return
+    execve(av[0], av, ev);
+    // If execve fails, write an error message to standard error and return 1
+    return (ft_putstr_2("error:: cannot execute ", av[0]));
 }
 
 int main(int ac, char **av, char **ev)
 {
-	int fd[2]; // An array to store two file descriptors for a pipe
-	int tmp_fd = dup(STDIN_FILENO); // Duplicate stdin to create a temporary file descriptor for input
-	int i = 0; // A variable to store the index of the current argument
-	(void)ac; // Ignore the unused argument count parameter
-	while (av[i] && av[i + 1]) // Loop through the arguments until the end or the second last argument
+	int fd[2];
+	int tmp_fd = dup(STDIN_FILENO); // Duplicate the standard input file descriptor
+	int i = 0;
+	(void)ac;
+
+	while (av[i] && av[i + 1])
 	{
-		av = &av[i + 1]; // Move the av pointer to the next argument after the semicolon or pipe character
-		i = 0; // Reset the index of the current argument to 0
-		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|")) // Loop through the arguments until the end or a semicolon or pipe character is found
+		// Move the pointer to the next command and reset the index
+		av = &av[i + 1];
+		i = 0;
+
+		// Find the index of the next pipe or semicolon in the command line
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
 			i++;
-		if (strcmp(av[0], "cd") == 0) // Check if the command is cd
+		// Check if the current command is a "cd" command
+		if (strcmp(av[0], "cd") == 0)
 		{
+			// Check that the "cd" command has exactly two arguments
 			if (i != 2)
-				ft_putstr_2("error: cd: bad arguments", NULL); // Print an error message if the number of arguments is not 2
+				ft_putstr_2("error: cd: bad arguments", NULL);
+			// Call the "chdir" system call to change the current working directory
 			else if (chdir(av[1]) != 0)
-				ft_putstr_2("error: cd: cannot change directory ", av[1]); // Print an error message if chdir returns an error
+				ft_putstr_2("error: cd: cannot change directory ", av[1]);
 		}
-		else if ((i != 0 && av[i] == NULL) || (i != 0 && strcmp(av[i], ";") == 0)) // Check if a semicolon character is found or the end of the argument list is reached
+		// If the current command is not a "cd" command
+		else if ((i != 0 && av[i] == NULL) || (i != 0 && strcmp(av[i], ";") == 0))
 		{
-			if (fork() == 0) // Create a child process to execute the command
+			// Fork a child process to execute the command
+			if (fork() == 0)
 			{
-				if (execute(av, i, tmp_fd, ev)) // Call the execute function and print an error message if it returns
+				// Call the "execute" function to execute the command and handle errors
+				if (execute(av, i, tmp_fd, ev))
 					return (1);
 			}
-			else // In the parent process
+			else
 			{
-				close(tmp_fd); // Close the temporary file descriptor
-				while (waitpid(-1, NULL, WUN
+				// Close the file descriptor for the temporary input file
+				close(tmp_fd);
+
+				// Wait for the child process to finish
+				while (waitpid(-1, NULL, WUNTRACED) != -1)
+					;
+
+				// Duplicate the standard input file descriptor to create a new temporary input file
+				tmp_fd = dup(STDIN_FILENO);
+			}
+		}
+		// If the current command is a pipe command
+		else if (i != 0 && strcmp(av[i], "|") == 0)
+		{
+			pipe(fd); // create a pipe
+			if (fork() == 0) // if child process
+			{
+				dup2(fd[1], STDOUT_FILENO); // redirect standard output to the write end of the pipe
+				close(fd[0]); // close the read end of the pipe
+				close(fd[1]); // close the write end of the pipe
+				if (execute(av, i, tmp_fd, ev)) // execute the left-hand side command and check for errors
+					return (1);
+			}
+			else // parent process
+			{
+				close(tmp_fd); // close the original standard input file descriptor
+				close(fd[1]); // close the write end of the pipe
+				tmp_fd = fd[0]; // set the new standard input file descriptor to the read end of the pipe
+			}
+		}
+	}
+	close(tmp_fd);
+	return 0;	
+}
+
